72ec3c0c46cc7cf00cc4a777e024574b1487e31a
givisab <givisab12345@gmail.com>
2023-07-31 23:23:13 +0400

    Add clear, color and resize functions

--- script.js
+++ script.js
@@ -1,14 +1,58 @@
 const container = document.querySelector(".canvasdiv");
-const color = "black";
+let color = "black";
+let size = 16;
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        container.appendChild(square);
+const resizeButton = document.querySelector(".size");
 
-        square.addEventListener("mouseover", function(){
-            square.style.backgroundColor = color;
-        });
+resizeButton.addEventListener("click", function(){
+    const sizeInput = document.getElementById("numberInput");
+    const canvasSize = sizeInput.value;
+    size = canvasSize;
+
+    const canvas = document.querySelector(".canvasdiv");
+    canvas.innerHTML = " ";
+    createCanvas(size);
+})
+
+const colorOptions = document.getElementById("colorPicker");
+
+colorOptions.addEventListener("change", function(){
+    const colorPicker = document.getElementById('colorPicker');
+    const selectedColor = colorPicker.value;
+    color = selectedColor;
+})
+
+const eraserButton = document.querySelector(".eraser");
+
+eraserButton.addEventListener("click", function(){
+    color = "white";
+});
+
+const clearButton = document.querySelector(".clear");
+
+clearButton.addEventListener("click", function(){
+    const canvas = document.querySelector(".canvasdiv");
+    canvas.innerHTML = " ";
+    createCanvas(size);
+});
+
+
+createCanvas(size);
+
+
+function createCanvas(num) {
+    const canvas = document.querySelector(".canvasdiv");
+    canvas.style.gridTemplateColumns = `repeat(${num}, 20px)`;
+
+    for (let i = 0; i < num; i++) {
+        for (let j = 0; j < num; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            container.appendChild(square);
+    
+            square.addEventListener("mouseover", function(){
+                square.style.backgroundColor = color;
+            });
+        }
     }
 }
