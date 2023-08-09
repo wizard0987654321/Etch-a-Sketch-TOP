61bfc4b5e35641ffc3e44392abfd7f99399d1d86
givisab <givisab12345@gmail.com>
2023-08-09 22:26:30 +0400

    Add comments, improve code style

--- script.js
+++ script.js
@@ -1,54 +1,62 @@
-const container = document.querySelector(".canvasdiv");
+// Global variables for initial canvas
+
+const canvas = document.querySelector(".canvasdiv");
 let color = "black";
 let size = 16;
 
 const resizeButton = document.querySelector(".size");
+
+// Giving user option to resize canvas
 
 resizeButton.addEventListener("click", function(){
     const sizeInput = document.getElementById("numberInput");
     const canvasSize = sizeInput.value;
     size = canvasSize;
 
-    const canvas = document.querySelector(".canvasdiv");
     canvas.innerHTML = " ";
     createCanvas(size);
 })
+
+// Letting the user to choose color
 
 const colorOptions = document.getElementById("colorPicker");
 
 colorOptions.addEventListener("change", function(){
-    const colorPicker = document.getElementById('colorPicker');
-    const selectedColor = colorPicker.value;
+    const selectedColor = colorOptions.value;
     color = selectedColor;
 })
 
+// Creating erase function, that is passed in html
+
 const eraserButton = document.querySelector(".eraser");
 
-eraserButton.addEventListener("click", function(){
+function erase() {
     color = "white";
-});
+};
+
+// Clearing the whole canvas
 
 const clearButton = document.querySelector(".clear");
 
 clearButton.addEventListener("click", function(){
-    const canvas = document.querySelector(".canvasdiv");
     canvas.innerHTML = " ";
     createCanvas(size);
 });
 
+/* Calling createCanvas function, in order to
+create the first, inital canvas */
 
 createCanvas(size);
 
 
 function createCanvas(num) {
-    const canvas = document.querySelector(".canvasdiv");
     canvas.style.gridTemplateColumns = `repeat(${num}, 20px)`;
 
     for (let i = 0; i < num; i++) {
         for (let j = 0; j < num; j++) {
             const square = document.createElement("div");
             square.classList.add("square");
-            container.appendChild(square);
+            canvas.appendChild(square);
     
             square.addEventListener("mouseover", function(){
                 square.style.backgroundColor = color;
